455a0570e5586b32fce09526ce0197de192277e1
bluzwar <36874000+bluzwar@users.noreply.github.com>
2023-09-17 00:07:07 -0400

    refactoring: separate pubsub and express/io

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-process.env.GOOGLE_APPLICATION_CREDENTIALS = '../json-keys/learning-gcp-396121-b153d54f5c8d.json';
-const topicNameOrId = 'projects/learning-gcp-396121/topics/topic-1';
-const subscriptionName = 'projects/learning-gcp-396121/subscriptions/topic-1-sub';
+const pubsub = require('./pubsub');
+const { server, io } = require('./app');
 
-const { join } = require('node:path');
-const express = require('express');
-const { createServer } = require('node:http');
-const { Server } = require('socket.io');
-const {PubSub} = require('@google-cloud/pubsub');
 const Events = require('events');
-
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-const pubSubClient = new PubSub(); 
-const subscription = pubSubClient.subscription(subscriptionName);
 const emitter = new Events.EventEmitter();
 
-async function publishMessage(topicNameOrId, data) {
-  const dataBuffer = Buffer.from(data);
-  try {
-    const messageId = await pubSubClient
-      .topic(topicNameOrId)
-      .publishMessage({data: dataBuffer});
-    console.log(`Message ${messageId} published.`);
-  } catch (error) {
-      console.error(`Received error while publishing: ${error.message}`);
-      process.exitCode = 1;
-  }
-}
 io.on('connection', (socket) => {
   console.log('user connected: ' + socket.id);
   socket.on('chat message', (msg) => {
     console.log('user: ' + socket.id + ' sent a chat message');
-    publishMessage(topicNameOrId, msg);
+    pubsub.publishMessage(msg);
   });
   emitter.on('chat message', (res) => {
     socket.emit('chat message', res.data.toString());
@@ -40,15 +16,9 @@ io.on('connection', (socket) => {
   });
 });
 
-subscription.on('message', message => {
+pubsub.subscription.on('message', message => {
   emitter.emit('chat message', message);
   message.ack();
-});
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '/public/index.html'));
 });
 
 server.listen(3000, () => {
---
+++ pubsub.js
@@ -0,0 +1,23 @@
+const {PubSub} = require('@google-cloud/pubsub');
+
+const topicNameOrId = 'projects/learning-gcp-396121/topics/topic-1';
+const subscriptionName = 'projects/learning-gcp-396121/subscriptions/topic-1-sub';
+
+const pubSubClient = new PubSub(); 
+const subscription = pubSubClient.subscription(subscriptionName);
+
+async function publishMessage(data) {
+    const dataBuffer = Buffer.from(data);
+    try {
+      const messageId = await pubSubClient
+        .topic(topicNameOrId)
+        .publishMessage({data: dataBuffer});
+      console.log(`Message ${messageId} published.`);
+    } catch (error) {
+        console.error(`Received error while publishing: ${error.message}`);
+        process.exitCode = 1;
+    }
+  }
+
+ exports.subscription = subscription;
+ exports.publishMessage = publishMessage;
